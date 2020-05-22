99f307edbda75797f0a226ad45a54150a5078ce3
YangSoYu <tjdwlsek123@gmail.com>
2020-05-22 11:10:29 +0900

    파일 시간정보 받아오는 함수 작성

--- main.c
+++ main.c
@@ -30,42 +30,29 @@ int main(){
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(){
-    stat("test1", &stat1);
+    
    // printf("#inod: %d\n",(int)stat1.st_ino);
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
-    stat("test2", &stat2);
+   
   //  printf("#inod: %d\n",(int)stat2.st_ino);
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+    time1 = localtime(&stat1.st_mtime);
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
+    time2 = localtime(&stat2.st_mtime);
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    int size1 =0; int size2=0;
-    size1 = (int)stat1.st_size;
-    size2 = (int)stat2.st_size;
-    
-    printf("size compare\n");
-    if(size1>size2){
-      printf("test1 is bigger\n\n");
-    }
-    else if(size1<size2){
-      printf("test2 is bigger\n\n");
-    }
-    else{
-      printf("sizes are equal\n\n");
-    }
+   
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
